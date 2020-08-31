985c9945aa24100f55958d78f004ddef79179ef8
krauserhuang <crescentdence@gmail.com>
2020-08-31 16:31:20 +0800

    Initial Commit

---
+++ TDEEPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TDEEPractice
+//
+//  Created by Tai Chin Huang on 2020/8/31.
+//  Copyright © 2020 Tai Chin Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
